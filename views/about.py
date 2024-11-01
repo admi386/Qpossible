2e5f552842abeb8e9f186ef16d44cfb048d5390c
Anatolii Pylypenko <adm.i386@gmail.com>
2024-11-01 21:28:19 +0100

    Create about.py
    Initial commit

---
+++ views/about.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("About")
+st.write("Qpossible v0.1.0")
+st.write("Home: [https://github.com/admi386/Qpossible](https://github.com/admi386/Qpossible)")
+st.write("License:  [Apache License 2.0](https://github.com/admi386/Qpossible?tab=Apache-2.0-1-ov-file#readme)")
